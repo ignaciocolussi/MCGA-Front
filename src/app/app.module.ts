1f16cf6d64dbe1da82e3098175a8d465f8fc2e44
ignaciocolussi <ignaciocolussi@outlook.com>
2024-03-09 13:01:02 -0300

    Se importa modulo public en app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicModule } from './public/public.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, PublicModule],
   providers: [],
   bootstrap: [AppComponent],
 })
