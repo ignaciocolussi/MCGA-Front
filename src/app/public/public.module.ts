d8d6300312362834567111c2a41dbb1674a2d83f
AlejandroGi <ale.322@live.com>
2024-03-09 13:09:36 -0300

    Se agrega el componente de NotFound

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -4,9 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [PublicComponent,LandingComponent,],
+  declarations: [
+    PublicComponent,
+    LandingComponent, 
+    NotFoundComponent],
   imports: [CommonModule, PublicRoutingModule, HttpClientModule],
 })
 export class PublicModule {}
