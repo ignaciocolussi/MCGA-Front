f40b69434a12506cc57f73c5e01dbfda64dfdc66
Alvaro Pistelli <alvaropistelli@outlook.com>
2024-03-09 13:05:44 -0300

    Se agrega el component de landing

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
-  declarations: [PublicComponent],
+  declarations: [PublicComponent,LandingComponent,],
   imports: [CommonModule, PublicRoutingModule, HttpClientModule],
 })
 export class PublicModule {}
