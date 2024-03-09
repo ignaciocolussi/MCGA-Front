20c1dc0092425d815bdb50b1c46900e71850c3e7
ignaciocolussi <ignaciocolussi@outlook.com>
2024-03-09 13:17:15 -0300

    Se genera componente de registro

---
+++ src/app/public/components/signup/signup.component.ts
@@ -0,0 +1,60 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PublicService } from '../../services/public.service';
+import { UserRegistration } from '../../models/user';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
+})
+export class SignupComponent {
+  public error!: string;
+  public signupForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    surname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    password2: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  constructor(private _publicService: PublicService, private _router: Router) {}
+
+  async onSubmit() {
+    if (this.signupForm.value.password !== this.signupForm.value.password2) {
+      this.error = 'Las contraseñas no coinciden';
+      return;
+    }
+
+    if (
+      this.signupForm.value.name &&
+      this.signupForm.value.surname &&
+      this.signupForm.value.email &&
+      this.signupForm.value.password
+    ) {
+      console.debug('Enviando datos al servidor...');
+      try {
+        const res = await this._publicService.postUser({
+          name: this.signupForm.value.name,
+          surname: this.signupForm.value.surname,
+          email: this.signupForm.value.email,
+          password: this.signupForm.value.password,
+        });
+
+        //console.debug(res);
+
+        this._router.navigateByUrl('/private/login');
+      } catch (error: any) {
+        console.error(error);
+        this.error = error.error.message;
+      }
+    }
+  }
+}
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -5,12 +5,22 @@ import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-
+import { SignupComponent } from './components/signup/signup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     PublicComponent,
-    LandingComponent, 
-    NotFoundComponent],
-  imports: [CommonModule, PublicRoutingModule, HttpClientModule],
+    LandingComponent,
+    NotFoundComponent,
+    SignupComponent,
+  ],
+  imports: [
+    CommonModule,
+    PublicRoutingModule,
+    HttpClientModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class PublicModule {}
