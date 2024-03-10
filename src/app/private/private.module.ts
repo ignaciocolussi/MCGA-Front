2f9adfaa8af684d6ce3bd34089094a4d2129eb5c
ignaciocolussi <ignaciocolussi@outlook.com>
2024-03-10 11:45:49 -0300

    Actualizo imports de componentes

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -3,10 +3,33 @@ import { CommonModule } from '@angular/common';
 
 import { PrivateRoutingModule } from './private-routing.module';
 import { PrivateComponent } from './private.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { NoteMiniComponent } from './components/note-mini/note-mini.component';
+import { TagComponent } from './components/tag/tag.component';
+import { NoteListComponent } from './components/note-list/note-list.component';
+import { NotesFilterComponent } from './components/notes-filter/notes-filter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
+import { NoteBlankComponent } from './components/note-blank/note-blank.component';
 
 @NgModule({
-  declarations: [PrivateComponent
+  declarations: [
+    PrivateComponent,
+    NavbarComponent,
+    HomeComponent,
+    NoteMiniComponent,
+    TagComponent,
+    NoteListComponent,
+    NotesFilterComponent,
+    NoteBlankComponent,
   ],
-  imports: [CommonModule, PrivateRoutingModule],
+  imports: [
+    CommonModule,
+    PrivateRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class PrivateModule {}
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -5,21 +5,23 @@ import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from '../private/components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SignupComponent } from './components/signup/signup.component';
+
 @NgModule({
   declarations: [
     PublicComponent,
     LandingComponent,
     NotFoundComponent,
-    SignupComponent,
     NavbarComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
     CommonModule,
     PublicRoutingModule,
-    HttpClientModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
