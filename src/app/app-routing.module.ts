023e2f5727a6a604e6fbd771951bc109ff3706cf
ignaciocolussi <ignaciocolussi@outlook.com>
2024-03-10 11:45:25 -0300

    Actualizo rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'private',
+    loadChildren: () =>
+      import('./private/private.module').then((m) => m.PrivateModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./public/public.module').then((m) => m.PublicModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
